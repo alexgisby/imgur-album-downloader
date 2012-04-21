16eb20c7057da156c9d6380693fc8e4450417134
Alex Gisby <alex.gisby@gmail.com>
2012-04-21 22:05:13 +0100

    Refactored into a sexy class type thing.

--- imguralbum.py
+++ imguralbum.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-imguralbum.py
+imguralbum.py - Download a whole imgur album in one go.
 
 MIT License
-
+Copyright Alex Gisby <alex@solution10.com>
 """
 
 import sys
@@ -13,14 +13,13 @@ import urllib
 import os
 
 help_message = '''
-
 Quickly and easily download an album from Imgur.
 
 Format:
-	python imguralbum.py [album URL] [destination folder]
+	$ python imguralbum.py [album URL] [destination folder]
 	
 Example:
-	python imguralbum.py http://imgur.com/a/uOOju#6 images
+	$ python imguralbum.py http://imgur.com/a/uOOju#6 /Users/alex/images
 	
 If you omit the dest folder name, the utility will create one with the same name as the
 album (for example for http://imgur.com/a/uOOju it'll create uOOju/ in the cwd)
@@ -28,51 +27,85 @@ album (for example for http://imgur.com/a/uOOju it'll create uOOju/ in the cwd)
 '''
 
 class ImgurAlbumException(Exception):
-	def __init__(self, msg):
-		print msg
+	def __init__(self, msg=False):
+		self.msg = msg
 
-args = sys.argv
 
-if len(args) == 1:
-	raise ImgurAlbumException("You must provide the album URL")
+class ImgurAlbumDownloader:
+	def __init__(self, album_url, output_messages=False):
+		"""
+		Constructor. Pass in the album_url. Seeing as this is mostly a shell tool, you can have the
+		class output messages too.
+		"""
+		self.album_url = album_url
+		self.output_messages = output_messages
+		
+		# Check the URL is actually imgur:
+		match = re.match('http\:\/\/(www\.)?imgur\.com/a/([a-zA-Z0-9]+)(#[0-9]+)?', album_url)
+		if not match:
+			raise ImgurAlbumException("URL must be a valid Imgur Album")
+		
+		self.album_key = match.group(2)
 
-albumURL = args[1]
+		# Read the no-script version of the page for all the images:
+		noscriptURL = 'http://imgur.com/a/' + match.group(2) + '/noscript'
+		self.response = urllib.urlopen(noscriptURL)
 
-# Check the URL is actually imgur:
-match = re.match('http\:\/\/(www\.)?imgur\.com/a/([a-zA-Z0-9]+)(#[0-9]+)?', albumURL)
-if not match:
-	raise ImgurAlbumException("URL must be a valid Imgur Album")
+		if self.response.getcode() != 200:
+			raise ImgurAlbumException("Error reading Imgur: Error Code %d" % self.response.getcode())
+	
+	def save_images(self, foldername = False):
+		"""
+		Saves the images from the album into a folder given by foldername.
+		If no foldername is given, it'll use the album key from the URL.
+		"""
+		html = self.response.read()
+		self.images = re.findall('<img src="(http\:\/\/i\.imgur\.com\/([a-zA-Z0-9]+\.(jpg|jpeg|png|gif)))"', html)
 
-print "Reading Album: " + albumURL
+		if self.output_messages:
+			print "Found %d images in album" % len(self.images)
 
-# We read from the noscript version of imgur to make sure all images are on the page:
-noscriptURL = 'http://imgur.com/a/' + match.group(2) + '/noscript'
-response = urllib.urlopen(noscriptURL)
+		# Try and create the album folder:
+		if foldername:
+			albumFolder = foldername
+		else:
+			albumFolder = self.album_key
 
-if response.getcode() != 200:
-	raise ImgurAlbumException("Error reading Imgur: %d" % response.getcode())
+		if not os.path.exists(albumFolder):
+			os.makedirs(albumFolder)
 
-print "Album found!"
+		# And finally loop through and save the images:
+		for image in self.images:
+			if self.output_messages:
+				print "Fetching Image: " + image[0]
+			path = os.path.join(albumFolder, image[1])
+			urllib.urlretrieve(image[0], path)
+		
+		if self.output_messages:
+			print ""
+			print "Done!"
+	
 
-html = response.read()
-images = re.findall('<img src="(http\:\/\/i\.imgur\.com\/([a-zA-Z0-9]+\.(jpg|jpeg|png|gif)))"', html)
+		
+if __name__ == '__main__':
+	args = sys.argv
 
-print "Found %d images in album" % len(images)
-
-# Try and create the album folder:
-if len(args) == 3:
-	albumFolder = args[2] 
-else:
-	albumFolder = match.group(2)
-
-if not os.path.exists(albumFolder):
-	os.makedirs(albumFolder)
-
-# And finally loop through and save the images:
-for image in images:
-	print "Fetching Image: " + image[0]
-	path = os.path.join(albumFolder, image[1])
-	urllib.urlretrieve(image[0], path)
-
-print ""
-print "All done!"
+	if len(args) == 1:
+		# Print out the help message and exit:
+		print help_message
+		exit()
+	
+	try:
+		# Fire up the class:
+		downloader = ImgurAlbumDownloader(args[1], output_messages=True)
+		
+		if len(args) == 3:
+			albumFolder = args[2] 
+		else:
+			albumFolder = False
+		
+		downloader.save_images(albumFolder)
+	except ImgurAlbumException as e:
+		print "Error: " + e.msg
+		print ""
+		print help_message
