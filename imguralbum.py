828e2cf59cff97a4e5bfc8ebdd5ecade99078f6d
Bikram Singh <bikram691995@gmail.com>
2017-05-08 22:27:34 +0530

    Update imguralbum.py
    prefix removed as set was unsorted and was downloading same pic with different names

--- imguralbum.py
+++ imguralbum.py
@@ -146,7 +146,7 @@ class ImgurAlbumDownloader:
                 int(math.ceil(math.log(len(self.imageIDs) + 1, 10))),
                 counter
             )
-            path = os.path.join(albumFolder, prefix + image[0] + image[1])
+            path = os.path.join(albumFolder, image[0] + image[1])
 
             # Run the callbacks:
             for fn in self.image_callbacks:
