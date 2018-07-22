9eb978551e551e4446c430f59a83f49eb0153533
James Curran <j.curran@auckland.ac.nz>
2018-07-22 17:18:43 +1200

    fixed __init__.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='alex@solution10.com',
     packages=find_packages(exclude=['tests*']),
     install_requires=['requests', 'pillow'],
-    version='0.1',
+    version='0.1-001',
     license='MIT',
     description='Download a whole Imgur album in one go',
     long_description=open('readme.md').read()
